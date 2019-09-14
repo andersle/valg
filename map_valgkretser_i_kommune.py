f754292924d1f9909bca09a542969f366e337ad0
Anders Lervik <andersle@gmail.com>
2019-09-14 20:42:29 +0200

    Updated script to work with the csv raw data.

--- map_valgkretser_i_kommune.py
+++ map_valgkretser_i_kommune.py
@@ -3,9 +3,13 @@
 """Create a map showing the largest party in different voting areas."""
 import pathlib
 import sys
-import pandas as pd
 import numpy as np
-from map_basics import produce_map, load_json_file, create_tool_tip
+from map_basics import (
+    produce_map,
+    load_json_file,
+    create_tool_tip,
+    read_csv_results,
+)
 
 
 VALGKRETS_DIR = pathlib.Path('valgkretser')
@@ -21,39 +25,59 @@ def _add_coordinates(feature, coordinates):
     coordinates.append(np.average(coords, axis=0))
 
 
-def get_geojson_data(result_files):
+def extract_data(results, kommune):
+    """Extract the data we want from the results."""
+    kommune_data = results[results['Kommunenummer'] == kommune].groupby(
+        ['Stemmekretsnummer']
+    )
+    area = {}
+    kretser = []
+    for name, group in kommune_data:
+        idx = group['Oppslutning prosentvis'].idxmax()
+        maxi = results.iloc[idx, :]
+        area[name] = {
+            'partinavn': maxi['Partinavn'],
+            'oppslutning': maxi['Oppslutning prosentvis'],
+            'krets': maxi['Stemmekretsnavn'],
+        }
+        kretser.append(maxi['Stemmekretsnavn'])
+    all_same = len(kretser) == 1 and kretser[0] == 'Hele kommunen'
+    return area, all_same
+
+
+def get_geojson_data(raw_data, kommuner):
     """Read in result files are produce corresponding geojson data."""
+    results = read_csv_results(raw_data)
+
     all_geojson_data = []
     coordinates = []
-    tooltip = []
-    for result_file in result_files:
-        print('Reading file "{}"'.format(result_file))
-        results = pd.read_json(result_file)
-        # Each file contains the result for a single municipality:
-        kommune = results['Kommunenummer'].tolist()[0]
-        kommune_navn = results['Kommunenavn'].tolist()[0]
-        # Check if we do actually have several voting areas or just
-        # one for the whole municipality:
-        use_all = results['Stemmekretsnavn'].tolist()[0] == 'Hele kommunen'
-        geojson_file = VALGKRETS_DIR.joinpath(
-            VALGKRETS.format(
-                str(kommune).rjust(4, '0')
-            )
+    tooltips = []
+    for kommune in kommuner:
+        # Get results for each voting area:
+        kommune_navn = results[
+            results['Kommunenummer'] == kommune
+        ]['Kommunenavn'].tolist()[0]
+        print('Reading data for "{}"'.format(kommune_navn))
+        area, all_same = extract_data(results, kommune)
+        # Read the geojson file for this kommune:
+        geojson_data = load_json_file(
+            VALGKRETS_DIR.joinpath(VALGKRETS.format(kommune))
         )
-        geojson_data = load_json_file(geojson_file)
-        row = results[results['Kommunenummer'] == kommune]
+        # Add results to the features:
         for feature in geojson_data['features']:
-            if not use_all:
-                # Update to new region:
-                nummer = feature['properties']['valgkretsnummer']
-                row = results[results['Stemmekretsnummer'] == nummer]
-            feature['properties']['partinavn'] = row['Partinavn'].tolist()[0]
+            if not all_same:
+                krets = str(
+                    feature['properties']['valgkretsnummer']
+                ).rjust(4, '0')
+            else:
+                krets = '0000'
+            feature['properties']['partinavn'] = area[krets]['partinavn']
             feature['properties']['oppslutning'] = '({:4.2f} %)'.format(
-                row['Oppslutning prosentvis'].tolist()[0]
+                area[krets]['oppslutning']
             )
             _add_coordinates(feature, coordinates)
         all_geojson_data.append((kommune_navn, geojson_data))
-        tooltip.append(
+        tooltips.append(
             create_tool_tip(
                 ('valgkretsnavn', 'partinavn', 'oppslutning'),
                 ('Valgkrets:', 'Største parti:', 'Oppslutning (%):'),
@@ -63,21 +87,22 @@ def get_geojson_data(result_files):
     map_settings = {
         'center': np.average(coordinates, axis=0)[::-1],
         'zoom': 10,
-        'tooltip': tooltip,
+        'tooltip': tooltips,
     }
     return all_geojson_data, map_settings
 
 
-def main(result_files):
+def main(raw_data, kommuner):
     """Read input files and create the map."""
-    geojson_data, map_settings = get_geojson_data(result_files)
-    if len(result_files) == 1:
-        filename = pathlib.Path(result_files[0]).stem
-        out = 'valgkretser-{}.html'.format(filename)
+    geojson_data, map_settings = get_geojson_data(raw_data, kommuner)
+    if len(kommuner) == 1:
+        out = 'valgkretser-{}-{}.html'.format(
+            kommuner[0], geojson_data[0][0]
+        )
     else:
         out = 'map-valgkretser.html'
     produce_map(geojson_data, map_settings, output=out)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main(sys.argv[1], sys.argv[2:])
